e233ecce24949eaa626c3c73790ccbf2055a2a27
Aaron Burciaga <146780951+BurciagaAA128@users.noreply.github.com>
2024-03-22 12:38:56 -0600

    Update pantalla2_0321.dart

--- lib/pantalla2_0321.dart
+++ lib/pantalla2_0321.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 
+// ignore: camel_case_types
 class Pantalla2_0321 extends StatelessWidget {
   const Pantalla2_0321({Key? key}) : super(key: key);
 
@@ -9,16 +10,53 @@ class Pantalla2_0321 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Pantalla 2 Burciaga0321"),
-        backgroundColor: Colors.purple,
+        title: const Text(
+          "Pantalla 2 Burciaga0321",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Color(0xfff6991f),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () {}, //Si presiona botón
-              child: Text("Pantalla"),
+            Container(
+              height: 130,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color(0xffd25c0d),
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(50),
+                  bottomLeft: Radius.circular(50),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xaa862c09),
+                    offset: Offset(9, 9),
+                    blurRadius: 6,
+                  ),
+                ],
+              ),
+              alignment: Alignment.center,
+              child: Text(
+                'Mi header',
+                style: TextStyle(
+                  fontSize: 38,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              child: const Text(
+                "Aaron Burciaga Cortés",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Container(
+              child: const Text(
+                "Header Mat. 20308051280321",
+                style: TextStyle(fontSize: 20),
+              ),
             )
           ],
         ),
