e0024d1b98aeaa9a236af8b34879185c18216d15
Aaron Burciaga <146780951+BurciagaAA128@users.noreply.github.com>
2024-03-22 12:45:59 -0600

    Create pantalla15_0321.dart

---
+++ lib/pantalla15_0321.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Pantalla15_0321 extends StatelessWidget {
+  const Pantalla15_0321({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pantalla 15 Burciaga0321",
+            style: TextStyle(color: Color(0xffffffff))),
+        backgroundColor: Color(0xff494949),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                color: Color(0xffb01414),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              width: 250,
+              height: 250,
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xff28b5c8),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                height: 100,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              child: const Text(
+                "Aaron Burciaga Cortés",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Container(
+              child: const Text(
+                "Figuras2 Mat. 20308051280321",
+                style: TextStyle(fontSize: 20),
+              ),
+            )
+          ], //Niños
+        ),
+      ),
+    );
+  } //Fin widget
+} //Fin pantalla1
